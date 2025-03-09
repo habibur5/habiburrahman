88c030404af2c51039b49850fa3ecdd1d646f0c9
MD. Habibur Rahman <habibur104828@gmail.com>
2025-03-09 21:35:08 +0600

    design portfolio website

---
+++ script.js
@@ -0,0 +1,8 @@
+const btn = document.getElementById('latest-works');
+btn.addEventListener("click", function() {
+    document.getElementById("works").scrollIntoView({ behavior: 'smooth' });
+});
+
+// configure send mail system
+
+
